16d66da5e91fc87095abbd50f47e76bad1badec1
Christian Johnson <charcoaldust@mac.com>
2023-02-01 20:09:54 -0800

    weather perfect

--- BasicJS/Making-Connections/script.js
+++ BasicJS/Making-Connections/script.js
@@ -2,24 +2,24 @@
 let recentCount = 12;
 let requestCount = 3;
 
-document.getElementById("recentCountBadge").innerText=recentCount;
-document.getElementById("requestCountBadge").innerText=requestCount;
+document.querySelector("#recentCountBadge").innerText=recentCount;
+document.querySelector("#requestCountBadge").innerText=requestCount;
 
 function changeName(element) {
-    document.getElementById("profileName").innerText='Wally Henerson';
+    document.querySelector("#profileName").innerText='Wally Henderson';
 };
 
 function acceptRequest(element){
     recentCount++;
     requestCount--;
-    document.getElementById("recentCountBadge").innerText=recentCount;
-    document.getElementById("requestCountBadge").innerText=requestCount;
+    document.querySelector("#recentCountBadge").innerText=recentCount;
+    document.querySelector("#requestCountBadge").innerText=requestCount;
     element.parentNode.parentNode.remove();
 
 };
 
 function denyRequest(element){
     requestCount--;
-    document.getElementById("requestCountBadge").innerText=requestCount;
+    document.querySelector("#requestCountBadge").innerText=requestCount;
     element.parentNode.parentNode.remove();
 };
--- BasicJS/weather/script.js
+++ BasicJS/weather/script.js
@@ -1,32 +1,25 @@
-console.log('JS reporting for duty!');
+//This makes sure the temperature units selector is set to 'Celcius'
 document.getElementById('temp-units-selector').value='celcius';
 
 function displayAlert(element) {
     alert("Loading weather report...")
 }
 
-function eatCookies(element) {
+function removeParent(element) {
     element.parentNode.remove()
 };
 
 function updateTemperatures(element) {
-    console.log('selection is hooked up')
-        let temperatureNumbers = document.querySelectorAll('.temp-number'); //yeah, I know it's a verbose variable but it helped me keep things straight in my head
-        let tempChoice = document.getElementById('temp-units-selector').value;
-        console.log(tempChoice);
-        
+        let temperatureNumbers = document.querySelectorAll('.temp-number');
+        let tempChoice = document.getElementById('temp-units-selector').value;        
 
-
-        //The problem (which I do not have time to solve) is that the rouning of numbers inflates the temperatures when switching back and forth.
-        // The pseudo code I can think of is displaying rounded numbers but preserving the original values in an another array 
-        //with wich we would use for the re-conversion
         if (tempChoice === 'farenhiet'){
             for(let i=0; i<temperatureNumbers.length; i++){
-                temperatureNumbers[i].innerText = Math.ceil((temperatureNumbers[i].innerText*1.8)+32)
+                temperatureNumbers[i].innerText = Math.round((temperatureNumbers[i].innerText*1.8)+32)
             }
         } else {
             for(let i=0; i<temperatureNumbers.length; i++){
-                temperatureNumbers[i].innerText = Math.ceil((temperatureNumbers[i].innerText-32)*.5556)
+                temperatureNumbers[i].innerText = Math.round((temperatureNumbers[i].innerText-32)*.5556)
             }
         }
 
